000d8b7c05c26d9548f0a3916b56d9e9a31a2cc1
Chris Carvajal <chriscarvajal10@gmail.com>
2018-12-16 19:33:36 -0300

    init: resumen de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+  nombre :string = "Ragnar Lodbrok";
+  nombre2 : string = "raGnar lodbRok";
+
+  arreglo :number[] = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+
+  moneda:number = 1245000.52;
+
+  heroe = {
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad: 500,
+    direccion:{
+      calle:"Primera",
+      casa: "19"
+    }
+  }
+
+  fecha = new Date();
+
+  video :string = "MwfNjjM-6ds";
+
+  activar:boolean = true;
+
+  valorDePromesa = new Promise(  (resolve,reject) =>{
+
+      setTimeout( ()=>resolve('Llego la data!'),3500);
+  }  )
 }
